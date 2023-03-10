f20b9718edfb86feacfc8c0f4fac56b3ea4688c1
Rico91130 <76157511+Rico91130@users.noreply.github.com>
2023-03-10 11:05:53 +0100

    Create Helper.js

---
+++ Helper.js
@@ -0,0 +1,47 @@
+const helper = (function () {
+
+    /* Convertit une string date au format GT en date JS */
+    function GTDate2JSDate(GTDate) {
+        var [dateValues, timeValues] = GTDate.split(' ');
+        var [day, month, year] = dateValues.split('-');
+        var [hours, minutes, seconds] = timeValues.split(':');
+        return new Date(+year, +month - 1, +day, +hours, +minutes, +seconds);
+    }
+
+    /* Convertit une date JS en date GT */
+    function JSDate2GTDate(JSDate) {
+        return JSDate.getFullYear() + "-" +  (""+(JSDate.getMonth()+1)).padStart(2,"0") + "-" + (""+JSDate.getDate()).padStart(2,"0")
+    }
+    
+
+    function downloadObjectAsCSV(exportObj, exportName) {
+        var dataStr = "data:text/csv;charset=utf-8,\ufeff" + encodeURIComponent(exportObj);
+        var downloadAnchorNode = document.createElement('a');
+        downloadAnchorNode.setAttribute("href", dataStr);
+        downloadAnchorNode.setAttribute("download", exportName);
+        document.body.appendChild(downloadAnchorNode); // required for firefox
+        downloadAnchorNode.click();
+        downloadAnchorNode.remove();
+    }
+
+    function buildQuery(url, params, defaultVal) {
+
+        var replaceURL = url;
+        Object.keys(params).forEach(key => {
+            replaceURL = replaceURL.replaceAll("%%" + key + "%%", params[key])
+        });
+
+        if (defaultVal == null)
+            defaultVal = "";
+
+        return replaceURL.replace(/%%(.*?)%%/ig, defaultVal);
+    }
+
+    return {
+        buildQuery : buildQuery,
+        downloadObjectAsCSV : downloadObjectAsCSV,
+        JSDate2GTDate : JSDate2GTDate,
+        GTDate2JSDate : GTDate2JSDate
+    }
+    
+})()
